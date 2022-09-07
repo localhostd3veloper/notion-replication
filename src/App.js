6f17c3404a90459feadcaf23055bf5901c832a3e
Gautam Anand <gautamanand022@gmail.com>
2022-09-07 23:51:44 +0530

    setup app for dev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+const HomePage = lazy(() => import("./components/HomePage"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
