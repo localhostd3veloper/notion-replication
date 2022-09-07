a371065cc8161feb9ac1654e30626767e11f3d3c
Gautam Anand <gautamanand022@gmail.com>
2022-09-08 02:08:49 +0530

    style: task

--- src/components/TaskView.js
+++ src/components/TaskView.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function TaskView({ view }) {
   return (
-    <div className="flex flex-col items-center gap-4">
-      <div className={`flex items-center justify-between w-48 font-semibold`}>
-        <div className="flex gap-2 text-gray-400">
+    <div className="flex flex-col items-center gap-4 w-48">
+      <div className={`flex items-center justify-between w-full font-semibold`}>
+        <div className="flex gap-2  text-gray-400">
           <div className={`text-slate-700 px-2 rounded-md ${view.color}`}>
             {view.name}
           </div>
@@ -12,14 +12,17 @@ function TaskView({ view }) {
         </div>
         <div className=" text-gray-400 cursor-pointer">+</div>
       </div>
-      {view.tasks.map((task) => (
-        <div
-          key={task.id}
-          className="flex w-48 gap-5 px-3 py-1 bg-white shadow rounded"
-        >
-          <div className="text-slate-700 font-semibold">{task.title}</div>
-        </div>
-      ))}
+      <div className="flex flex-col w-full gap-1.5 flex-nowrap">
+        {view.tasks.map((task) => (
+          <div
+            key={task.id}
+            className="flex px-3 py-2 bg-white shadow rounded w-full"
+          >
+            <div className="text-slate-700 font-semibold">{task.title}</div>
+          </div>
+        ))}
+        <div className="text-gray-400 text-sm mt-2">+ New</div>
+      </div>
     </div>
   );
 }
