e2322c64076428e7a6957f893c4200bd6a38f006
Gautam Anand <gautamanand022@gmail.com>
2022-09-09 02:49:11 +0530

    create: Custom Hook

--- src/components/TaskView.js
+++ src/components/TaskView.js
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
+import { useTask } from "../context/AppContext";
+
 function TaskView({ view }) {
   const [isInputVisible, setIsInputVisible] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const task = useTask();
+
   const handleNewTask = () => {
-    setIsInputVisible(true);
+    if (inputValue) {
+      task.addTask(inputValue, view.id);
+      setInputValue("");
+    }
+    document.getElementById("input").focus();
   };
 
   return (
-    <div className="flex flex-col items-center gap-4 w-48">
+    <div id="" className="flex flex-col items-center gap-4 w-48">
       <div className={`flex items-center justify-between w-full font-semibold`}>
         <div className="flex gap-2  text-gray-400">
           <div className={`text-slate-700 px-2 rounded-md ${view.color}`}>
@@ -32,17 +41,30 @@ function TaskView({ view }) {
         {isInputVisible && (
           <div className="flex px-3 py-1.5 bg-white drop-shadow-sm rounded w-full cursor-pointer border border-gray-200">
             <input
+              id="input"
               type="text"
               className="w-full text-slate-700 placeholder:font-normal font-semibold text-sm outline-none"
               placeholder="Type a name..."
               autoFocus
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleNewTask();
+                }
+              }}
+              onBlur={() => {
+                setIsInputVisible(false);
+              }}
             />
           </div>
         )}
 
         <button
           className="text-gray-400 text-left pl-2 text-sm mt-2 cursor-pointer focus:outline-blue-400/80"
-          onClick={handleNewTask}
+          onClick={() => setIsInputVisible(true)}
         >
           + New
         </button>
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,48 +1,52 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useContext } from "react";
 
 export const AppContext = createContext();
 
-export const AppProvider = ({ children }) => {
-  const [tasks] = useState([
-    {
-      id: 1,
-      title: "Task 1",
-      description: "Description 1",
-    },
-    {
-      id: 2,
-      title: "Task 2",
-      description: "Description 2",
-    },
-    {
-      id: 3,
-      title: "Task 3",
-      description: "Description 3",
-    },
-    {
-      id: 4,
-      title: "Task 4",
-      description: "Description 4",
-    },
-  ]);
+export function useTask() {
+  return useContext(AppContext);
+}
 
-  const [viewItems] = useState([
-    { name: "Not started", id: 1, color: "bg-red-300/20", tasks: tasks },
+export const AppProvider = ({ children }) => {
+  // const [tasks] = useState([
+  //   {
+  //     id: 1,
+  //     title: "Task 1",
+  //     description: "Description 1",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Task 2",
+  //     description: "Description 2",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Task 3",
+  //     description: "Description 3",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Task 4",
+  //     description: "Description 4",
+  //   },
+  // ]);
+
+  const [viewItems, setViewItems] = useState([
+    { name: "Not started", id: 1, color: "bg-red-300/20", tasks: [] },
     {
       name: "To Do",
       id: 2,
       color: "bg-blue-300/20",
       tasks: [
-        {
-          id: 1,
-          title: "Task 1",
-          description: "Description 1",
-        },
-        {
-          id: 2,
-          title: "Task 2",
-          description: "Description 2",
-        },
+        // {
+        //   id: 1,
+        //   title: "Task 1",
+        //   description: "Description 1",
+        // },
+        // {
+        //   id: 2,
+        //   title: "Task 2",
+        //   description: "Description 2",
+        // },
       ],
     },
     { name: "In progress", id: 3, color: "bg-yellow-300/20", tasks: [] },
@@ -51,16 +55,32 @@ export const AppProvider = ({ children }) => {
       id: 4,
       color: "bg-green-300/20",
       tasks: [
-        {
-          id: 5,
-          title: "Task 5",
-          description: "Description 5",
-        },
+        // {
+        //   id: 5,
+        //   title: "Task 5",
+        //   description: "Description 5",
+        // },
       ],
     },
   ]);
 
-  return (
-    <AppContext.Provider value={{ viewItems }}>{children}</AppContext.Provider>
-  );
+  const addTask = (title, viewId) => {
+    const newTask = {
+      id: Math.floor(Math.random() * 10000),
+      title,
+      description: "",
+    };
+
+    const newViewItems = viewItems.map((view) =>
+      view.id === viewId ? { ...view, tasks: [...view.tasks, newTask] } : view
+    );
+    setViewItems(newViewItems);
+  };
+
+  const value = {
+    viewItems,
+    addTask,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
